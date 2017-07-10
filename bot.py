33ae193f3691288e77c2d82e3b2e594b8316e3aa
AJ Snarr <ajsnarr98@gmail.com>
2017-07-09 22:49:48 -0400

    Added files for first working version of bot

---
+++ bot.py
@@ -0,0 +1,89 @@
+import asyncio
+import logging
+import os
+import sys
+import time
+import traceback
+
+import discord
+
+import accounts
+import message_parser
+import secret
+from utils import RestartRequired
+
+
+# set up logger
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# define parser variable; initialize as MessageParser
+parser = None
+
+class DiscordBot(discord.Client):
+
+    def __init__(self, *args, loop=None, **options):
+        super().__init__(*args, loop=None, **options)
+        self.parser = message_parser.MessageParser(self, logger)
+
+    async def on_ready(self):
+        print('Connected!')
+        print('Username: ' + self.user.name)
+        print('ID: ' + self.user.id)
+
+    async def on_message(self, message):
+        if message.author != self.user: # do not want bot to reply to self
+            await self.parser.parse_and_react(message)
+
+    def restart(self):
+        """ Restarts after event loop has ended """
+        seconds_before_restart = 5
+
+        pre_wait_message = 'event loop has stopped... waiting {sec} seconds before restart'
+        restarting_message = 'restarting bot...'
+
+        logger.debug(pre_wait_message.format(sec=seconds_before_restart))
+        time.sleep(seconds_before_restart)
+        logger.debug(restarting_message)
+
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+    def run(self, *args, **kwargs):
+        """ 
+        Can call commands.restart() to stop event loop """
+
+        restart = True
+
+        try:
+            self.loop.run_until_complete(self.start(*args, **kwargs))
+
+        except KeyboardInterrupt:
+            self.loop.run_until_complete(self.logout())
+            pending = asyncio.Task.all_tasks(loop=self.loop)
+            gathered = asyncio.gather(*pending, loop=self.loop)
+            try:
+                gathered.cancel()
+                self.loop.run_until_complete(gathered)
+
+                # we want to retrieve any exceptions to make sure that
+                # they don't nag us about it being un-retrieved.
+                gathered.exception()
+            except:
+                pass
+            # do not want to restart if given a KeyboardInterrupt
+            restart = False
+        except SystemExit:
+            restart = False
+        finally:
+            self.loop.close()
+            if restart:
+                self.restart()
+
+
+if __name__ == '__main__':
+    bot = DiscordBot()
+    bot.run(secret.botToken)
