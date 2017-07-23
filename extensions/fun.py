cd5645911f0ff37db084405352d2685eea275680
AJ Snarr <ajsnarr98@gmail.com>
2017-07-23 02:17:57 -0400

    Added support for adding custom 8ball responses

--- extensions/fun.py
+++ extensions/fun.py
@@ -20,32 +20,46 @@ class Fun():
 
             Gives a random 8ball response to the provided question.
         """
-        response_emoji_list = [':blush:',
-                               ':smirk:',
-                               ':ok_hand:',
-                               ':thinking_face:',
-                               ':rolling_eyes:']
+        eight_ball_filename = '8ball.json'
 
-        response_list = ['It is certain',
-                         'It is decidedly so',
-                         'Without a doubt',
-                         'Yes definitely',
-                         'You may rely on it',
-                         'As I see it, yes',
-                         'Most likely',
-                         'Outlook good',
-                         'Yes',
-                         'Signs point to yes',
-                         'Reply hazy try again',
-                         'Ask again later',
-                         'Better not tell you now',
-                         'Cannot predict now',
-                         'Concentrate and ask again',
-                         'Don\'t count on it',
-                         'My reply is no',
-                         'My sources say no',
-                         'Outlook not so good',
-                         'Very doubtful']
+        responses = self.data_man.load_json(eight_ball_filename)
+        response_list = None
+        response_emoji_list = None
+
+        if responses:
+            response_list = responses.get('response_list', None)
+            response_emoji_list = responses.get('emoji_list', None)
+        else:
+            response_emoji_list = [':blush:',
+                                   ':smirk:',
+                                   ':ok_hand:',
+                                   ':thinking_face:',
+                                   ':rolling_eyes:']
+            response_list = ['It is certain',
+                             'It is decidedly so',
+                             'Without a doubt',
+                             'Yes definitely',
+                             'You may rely on it',
+                             'As I see it, yes',
+                             'Most likely',
+                             'Outlook good',
+                             'Yes',
+                             'Signs point to yes',
+                             'Reply hazy try again',
+                             'Ask again later',
+                             'Better not tell you now',
+                             'Cannot predict now',
+                             'Concentrate and ask again',
+                             'Don\'t count on it',
+                             'My reply is no',
+                             'My sources say no',
+                             'Outlook not so good',
+                             'Very doubtful']
+
+            responses = {'response_list' : response_list, 
+                         'emoji_list' : response_emoji_list}
+
+            self.data_man.save_json(responses, eight_ball_filename)
 
         response = '[**8Ball**] :crystal_ball: {0} {1}'.format(
             random.choice(response_list), random.choice(response_emoji_list))
